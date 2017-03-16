ca681281a142627ce4a9e53455ddd061a2e22e97
Robby Ronk <robby.ronk@gmail.com>
2017-03-16 22:44:08 +1300

    Remove registration and session management

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,7 +1,5 @@
 import {combineReducers} from "redux";
 import {routerReducer} from "react-router-redux";
-import session from "./session";
-import registrations from "./registrations";
 import items from "./items";
 import focus from './focus'
 
@@ -9,6 +7,4 @@ export default combineReducers({
   routing: routerReducer,
   focus,
   items,
-  session,
-  registrations,
 });
