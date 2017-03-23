91c6809436fab47a3e18ccff73cd2b228610dfc5
Robby Ronk <robby.ronk@gmail.com>
2017-03-23 18:55:53 +1300

    Use 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -3,61 +3,61 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 const PATHS = {
-    app: path.join(__dirname, 'js'),
-    build: path.join(__dirname, 'build'),
+  app: path.join(__dirname, 'js'),
+  build: path.join(__dirname, 'build'),
 };
 
 const baseConfig = {
-    entry: './js/application.js',
-    output: {
-        path: PATHS.build,
-        filename: '[name].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './index.html'}),
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    cacheDirectory: true,
-                    plugins: ['transform-decorators-legacy', "react-hot-loader/babel"],
-                    presets: ['react', 'es2015', 'stage-2', 'stage-0'],
-                },
-            },
-        ]
-    }
+  entry: './js/application.js',
+  output: {
+    path: PATHS.build,
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'}),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          plugins: ['transform-decorators-legacy', "react-hot-loader/babel"],
+          presets: ['react', 'es2015', 'stage-2', 'stage-0'],
+        },
+      },
+    ]
+  }
 };
 
 const productionConfig = () => baseConfig;
 
 const developmentConfig = () => {
-    const config = {
-        devServer: {
-            // hotOnly: true, // uncomment to debug HMR, prevents a full refresh
-            hot: true, // tries to hot reload, will fallback to full refresh
-            historyApiFallback: true,
-            stats: 'errors-only',
-        },
-        plugins: [
-            new HtmlWebpackPlugin({ template: './index.html'}),
-            new webpack.HotModuleReplacementPlugin(),
-            new webpack.NamedModulesPlugin(),
-        ],
-    };
+  const config = {
+    devServer: {
+      // hotOnly: true, // uncomment to debug HMR, prevents a full refresh
+      hot: true, // tries to hot reload, will fallback to full refresh
+      historyApiFallback: true,
+      stats: 'errors-only',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: './index.html'}),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+    ],
+  };
 
-    return Object.assign({}, baseConfig, config)
+  return Object.assign({}, baseConfig, config)
 };
 
 module.exports = (env) => {
-    console.log('env', env);
+  console.log('env', env);
 
-    if (env === 'production') {
-        return productionConfig();
-    }
+  if (env === 'production') {
+    return productionConfig();
+  }
 
-    return developmentConfig();
+  return developmentConfig();
 };
