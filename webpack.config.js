26decdb93114e87989073c78aeaa6a392e7978c6
Robby Ronk <robby.ronk@gmail.com>
2017-03-23 18:49:34 +1300

    Configure webpack for hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 const PATHS = {
     app: path.join(__dirname, 'js'),
     build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const baseConfig = {
     entry: './js/application.js',
     output: {
         path: PATHS.build,
@@ -23,10 +24,40 @@ module.exports = {
                 exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,
-                    plugins: ['transform-decorators-legacy'],
+                    plugins: ['transform-decorators-legacy', "react-hot-loader/babel"],
                     presets: ['react', 'es2015', 'stage-2', 'stage-0'],
                 },
             },
         ]
     }
+};
+
+const productionConfig = () => baseConfig;
+
+const developmentConfig = () => {
+    const config = {
+        devServer: {
+            // hotOnly: true, // uncomment to debug HMR, prevents a full refresh
+            hot: true, // tries to hot reload, will fallback to full refresh
+            historyApiFallback: true,
+            stats: 'errors-only',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: './index.html'}),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin(),
+        ],
+    };
+
+    return Object.assign({}, baseConfig, config)
+};
+
+module.exports = (env) => {
+    console.log('env', env);
+
+    if (env === 'production') {
+        return productionConfig();
+    }
+
+    return developmentConfig();
 };
