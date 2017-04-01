87d4297795cf464350136a70190a938f0baa3a13
Robby Ronk <robby.ronk@gmail.com>
2017-04-02 10:56:47 +1200

    Make the modal a little kinder to the user

--- js/components/export-modal.js
+++ js/components/export-modal.js
@@ -19,16 +19,15 @@ const mapDispatchToProps = {
 export default class ExportModal extends React.Component {
   render() {
     return (
-      <Modal isOpen={this.props.isExporting} toggle={this.toggle} className={this.props.className}>
-        <ModalHeader toggle={this.toggle}>Export Your Zone</ModalHeader>
+      <Modal isOpen={this.props.isExporting} toggle={this.props.close} className={this.props.className}>
+        <ModalHeader toggle={this.props.close}>Export Your Zone</ModalHeader>
         <ModalBody>
           <textarea className="form-control" name="items" id="items" rows="10">
             {JSON.stringify(this.props.items, null, 2)}
           </textarea>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-          <Button color="secondary" onClick={this.props.close}>Cancel</Button>
+          <Button color="primary" onClick={this.props.close}>Done</Button>
         </ModalFooter>
       </Modal>
     );
