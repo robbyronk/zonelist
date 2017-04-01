007700fc3ec7e5ad7d5e0bf39219d2a00060eb4d
Robby Ronk <robby.ronk@gmail.com>
2017-04-02 11:35:49 +1200

    Close modal when setting items

--- js/reducers/is.js
+++ js/reducers/is.js
@@ -8,6 +8,8 @@ export default function reducer(state = initialState, action = {}) {
       return update(state, {importing: {$set: true}})
     case 'CLOSE_IMPORT_MODAL':
       return update(state, {importing: {$set: false}})
+    case 'SET_ITEMS':
+      return update(state, {importing: {$set: false}})
     case 'OPEN_EXPORT_MODAL':
       return update(state, {exporting: {$set: true}})
     case 'CLOSE_EXPORT_MODAL':
