06c0edea293a8cd5f8d4981cb0c556dc888310ed
Robby Ronk <robby.ronk@gmail.com>
2017-04-02 10:48:28 +1200

    copy/paste bug

--- js/reducers/is.js
+++ js/reducers/is.js
@@ -9,9 +9,9 @@ export default function reducer(state = initialState, action = {}) {
     case 'CLOSE_IMPORT_MODAL':
       return update(state, {importing: {$set: false}})
     case 'OPEN_EXPORT_MODAL':
-      return update(state, {importing: {$set: true}})
+      return update(state, {exporting: {$set: true}})
     case 'CLOSE_EXPORT_MODAL':
-      return update(state, {importing: {$set: false}})
+      return update(state, {exporting: {$set: false}})
 
     default:
       return state;
