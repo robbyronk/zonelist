6ef9c52ff79c627e86890b629bb1c5ab420ea600
Robby Ronk <robby.ronk@gmail.com>
2017-04-02 18:56:18 +1200

    Make the drag handle nicer

--- js/components/Item.js
+++ js/components/Item.js
@@ -74,7 +74,7 @@ export default class Item extends Component {
     return connectDropTarget(connectDragPreview(
       <div className={classNames({dragging: isDragging})}>
         <div className="item">
-        {connectDragSource(<span className="drag-source">&#x2195;</span>)}
+        {connectDragSource(<span className="drag-source badge badge-success">&#x2195;</span>)}
           <ItemTitle
             id={id}
           />
