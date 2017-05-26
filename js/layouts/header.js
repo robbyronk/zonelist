4f0337d73c8f899d32d11d1fbea4fbd05987aee7
Robby Ronk <robby.ronk@gmail.com>
2017-05-26 22:48:31 +1200

    Make a top menu for import / export, show only one list or board button

--- js/layouts/header.js
+++ js/layouts/header.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {reset, openImportModal, openExportModal, showBoard, hideBoard} from '../actions'
 import ImportModal from '../components/import-modal'
 import ExportModal from '../components/export-modal'
+import {DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown} from 'reactstrap'
 
 const mapDispatchToProps = {
   openExportModal,
@@ -12,7 +13,11 @@ const mapDispatchToProps = {
   hideBoard,
 }
 
-@connect(null, mapDispatchToProps)
+const mapStateToProps = (state) => ({
+  isBoard: state.is.board
+})
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class Header extends React.Component {
   render() {
     return (
@@ -20,25 +25,32 @@ export default class Header extends React.Component {
         <div className="col-3">
           <h1>ZoneList</h1>
         </div>
-        <div className="col-3">
-          <button className="btn btn-block btn-danger" onClick={this.props.reset}>Reset</button>
-        </div>
-        <div className="col-3">
-          <button className="btn btn-block" onClick={this.props.openImportModal}>Import</button>
+        <div className="col-9 text-right">
+          <UncontrolledDropdown>
+            <DropdownToggle>
+              Import / Export
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem onClick={this.props.openImportModal}>Import</DropdownItem>
+              <DropdownItem onClick={this.props.openExportModal}>Export</DropdownItem>
+              <DropdownItem divider/>
+              <DropdownItem header>Careful!</DropdownItem>
+              <DropdownItem header>Reset will erase everything!</DropdownItem>
+              <DropdownItem onClick={this.props.reset}>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
           <ImportModal/>
-        </div>
-        <div className="col-3">
-          <button className="btn btn-block" onClick={this.props.openExportModal}>Export</button>
           <ExportModal/>
         </div>
         <div className="col-6">
           version 0.0.0-submarine-screendoor
         </div>
-        <div className="col-3">
-          <button className="btn btn-block" onClick={this.props.hideBoard}>Show List</button>
-        </div>
-        <div className="col-3">
-          <button className="btn btn-block" onClick={this.props.showBoard}>Show Board</button>
+        <div className="col-6 text-right">
+          {
+            this.props.isBoard
+              ? <button className="btn" onClick={this.props.hideBoard}>Show List</button>
+              : <button className="btn" onClick={this.props.showBoard}>Show Board</button>
+          }
         </div>
       </div>
     )
