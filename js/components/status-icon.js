2077bcea22da656667d22d912fa1f4b092d3727f
Robby Ronk <robby.ronk@gmail.com>
2017-05-26 21:26:42 +1200

    Create component to show a status icon

---
+++ js/components/status-icon.js
@@ -0,0 +1,22 @@
+import React, {Component, PropTypes} from 'react'
+import classNames from 'classnames'
+
+export default class StatusIcon extends Component {
+  static propTypes = {
+    status: PropTypes.string,
+    className: PropTypes.string,
+  };
+
+  render() {
+    const statusToIcon = {
+      toDo: 'list-alt',
+      inProgress: 'crosshairs',
+      waiting: 'clock-o',
+      done: 'check'
+    }
+    const icon = statusToIcon[this.props.status || 'toDo']
+    return (
+      <i className={classNames('fa', `fa-${icon}`, this.props.className)} aria-hidden="true"/>
+    )
+  }
+}
