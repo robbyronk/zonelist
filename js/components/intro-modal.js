b8fe63f470d746c8905298a1ddd3a572e1dc1e14
Robby Ronk <robby.ronk@gmail.com>
2017-05-30 20:25:41 +1200

    Add text to the intro modal

--- js/components/intro-modal.js
+++ js/components/intro-modal.js
@@ -19,12 +19,22 @@ export default class IntroModal extends React.Component {
   render() {
     return (
       <Modal isOpen={this.props.isOpen} toggle={this.props.close} className={this.props.className}>
-        <ModalHeader toggle={this.props.close}>Welcome to ZoneList!</ModalHeader>
+        <ModalHeader toggle={this.props.close}>Welcome to ZoneList</ModalHeader>
         <ModalBody>
-          <p>This is the introduction to ZoneList</p>
+          <p>
+            Break your challenge down in to tasks and then break those tasks down into smaller tasks using
+            the <b>outline</b>. Then, pick one to be your <b>main focus</b> and finish it. Pick another until you
+            finish.
+          </p>
+          <p>
+            When you get distracted, don't fret! Your <b>main focus</b> will be highlighted when you get back.
+          </p>
+          <p>
+            If plans change, no worries. Just go back to the <b>outline</b> and change the plan.
+          </p>
         </ModalBody>
         <ModalFooter>
-          <Button color="success" onClick={this.props.close}>Get Started</Button>
+          <Button color="success" onClick={this.props.close}>Get Started!</Button>
         </ModalFooter>
       </Modal>
     );
