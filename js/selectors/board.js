855e995c333ecd9de217d95af6ba5f3b9a99c4e5
Robby Ronk <robby.ronk@gmail.com>
2017-06-05 02:35:52 +1200

    cleanup

--- js/selectors/board.js
+++ js/selectors/board.js
@@ -1,17 +1,13 @@
-// todo write selector for each lane
 import {createSelector} from 'reselect'
 import {every, filter, includes} from 'lodash'
-// to do - items which have only done children (or no children)
-// in progress - items with the status of in progress
-// waiting - items with the status of in progress
-// done - items with the status of done
 
 const items = state => state.items
 
 export const toDoLane = createSelector(
   items,
-  items => filter(items, i => every(i.children, c => items[c].status === 'done') &&
-  !includes(['inProgress', 'waiting', 'done'], i.status))
+  items => filter(
+    items,
+    i => every(i.children, c => items[c].status === 'done') && !includes(['inProgress', 'waiting', 'done'], i.status))
 )
 
 const statusSelector = status => createSelector(
