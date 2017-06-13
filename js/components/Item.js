69929eae8d8aab069fdc6f810ad925931057b08e
Robby Ronk <robby.ronk@gmail.com>
2017-06-13 23:50:07 +1200

    Mock up list item toolbar

--- js/components/Item.js
+++ js/components/Item.js
@@ -75,6 +75,26 @@ export default class Item extends Component {
           <div className="item-status"><ItemStatusDropdown item={item}/></div>
           <ItemTitle id={id}/>
         </div>
+        <div className="item-toolbar">
+          <button className="btn btn-sm mx-2">
+            <i className="fa fa-level-up" aria-hidden="true"/>
+          </button>
+          <button className="btn btn-sm mx-2">
+            <i className="fa fa-level-down" aria-hidden="true"/>
+          </button>
+          <button className="btn btn-sm mx-2">
+            <i className="fa fa-arrow-up" aria-hidden="true"/>
+          </button>
+          <button className="btn btn-sm mx-2">
+            <i className="fa fa-arrow-down" aria-hidden="true"/>
+          </button>
+          <button className="btn btn-sm mx-2">
+            <i className="fa fa-plus" aria-hidden="true"/>
+          </button>
+          <button className="btn btn-sm right">
+            <i className="fa fa-trash" aria-hidden="true"/>
+          </button>
+        </div>
         <Tree parent={id} ids={item.children}/>
       </div>
     ))
