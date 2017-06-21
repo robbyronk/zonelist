7731e219ff3f15d62313c0ece16129f225947ef0
Robby Ronk <robby.ronk@gmail.com>
2017-06-22 11:21:40 +1200

    Update the main layout component to show the new outline view

--- js/components/main.js
+++ js/components/main.js
@@ -3,14 +3,25 @@ import Header from './header'
 import {connect} from 'react-redux'
 import ZoneList from './list'
 import FocusList from './focus'
+import Outline from './outline'
 
 @connect(state => ({view: state.view}))
 export default class MainLayout extends React.Component {
+  _component = (view) => {
+    switch (view) {
+      case 'old-outline':
+        return (<ZoneList/>)
+      case 'old-focus':
+        return (<FocusList/>)
+    }
+    return (<Outline/>)
+  }
+
   render() {
     return (
       <div className="container">
         <Header/>
-        {this.props.view === 'focus' ? <FocusList/> : <ZoneList/>}
+        {this._component(this.props.view)}
       </div>
     );
   }
