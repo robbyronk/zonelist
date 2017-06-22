9e8f069a6b4ab18bc99e9cac8b8fb5ee4710e75c
Robby Ronk <robby.ronk@gmail.com>
2017-06-22 16:06:07 +1200

    Show main focus and wire up buttons

--- js/components/focus/main-focus.js
+++ js/components/focus/main-focus.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import {setStatus} from '../../actions'
+import {head} from 'lodash'
+import {inProgressLane} from '../../selectors/board'
+import {connect} from 'react-redux'
+import TaskContext from './task-context'
 
-function Focus(props) {
+function MainFocus({task, setStatus}) {
+  const {id} = task
   return (
     <div className="container">
       <div className="alert alert-success text-center" role="alert">
@@ -8,27 +14,32 @@ function Focus(props) {
       </div>
       <div className="row mb-1">
         <div className="col-12">
-          <div className="top-level"><span>Big Project</span>
-            <div className="tree"><h5>Task Two</h5>
-              <div className="tree"><h4>Smaller Task</h4></div>
-            </div>
-          </div>
+          <TaskContext id={id}/>
         </div>
       </div>
       <div className="row">
         <div className="col-sm-6 flex-sm-last mb-1">
-          <button className="btn btn-block btn-success"><i className="fa fa-check"/> Finished!</button>
+          <button className="btn btn-block btn-success" onClick={() => setStatus(id, 'done')}>
+            <i className="fa fa-check"/> Finished!
+          </button>
         </div>
         <div className="col-sm-3 mb-1">
-          <button className="btn btn-block btn-warning"><i className="fa fa-clock-o"/> Snooze</button>
+          <button className="btn btn-block btn-warning" onClick={() => setStatus(id, 'waiting')}>
+            <i className="fa fa-clock-o"/> Snooze
+          </button>
         </div>
         <div className="col-sm-3 flex-sm-first mb-1">
-          <button className="btn btn-block"><i className="fa fa-undo"/> Un-focus</button>
+          <button className="btn btn-block" onClick={() => setStatus(id, 'toDo')}>
+            <i className="fa fa-undo"/> Un-focus
+          </button>
         </div>
       </div>
     </div>
   )
 }
+const mapStateToProps = state => ({
+    task: head(inProgressLane(state)),
+  }
+)
 
-export default Focus
-// export default connect(null, {actions})(Focus)
+export default connect(mapStateToProps, {setStatus})(MainFocus)
