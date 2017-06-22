0e23b35a5169a1886c587f736210fcf06edc2994
Robby Ronk <robby.ronk@gmail.com>
2017-06-22 21:55:05 +1200

    Ignore unselecting tasks action for now

--- js/reducers/outline.js
+++ js/reducers/outline.js
@@ -9,8 +9,9 @@ export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case ActionTypes.SELECT_TASK:
       return update(state, {selectedItem: {$set: action.id}})
-    case ActionTypes.UNSELECT_TASK:
-      return update(state, {selectedItem: {$set: null}})
+    // TODO will need to unselect tasks sometimes?
+    // case ActionTypes.UNSELECT_TASK:
+    //   return update(state, {selectedItem: {$set: null}})
 
     default:
       return state;
