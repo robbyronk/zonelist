7a7f6f8b680bf75726b92dbd12bcd8c490ec17b6
Robby Ronk <robby.ronk@gmail.com>
2017-06-24 13:10:37 +1200

    Refactor status icon into a stateless functional component

--- js/components/status-icon.js
+++ js/components/status-icon.js
@@ -1,23 +1,23 @@
-import React, {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 import classNames from 'classnames'
 
-export default class StatusIcon extends Component {
-  static propTypes = {
-    status: PropTypes.string,
-    className: PropTypes.string,
-    style: PropTypes.string,
-  };
-
-  render() {
-    const statusToIcon = {
-      toDo: 'list-alt',
-      inProgress: 'star text-warning',
-      waiting: 'clock-o',
-      done: 'check'
-    }
-    const icon = statusToIcon[this.props.status || 'toDo']
-    return (
-      <i className={classNames('fa', 'fa-lg', `fa-${icon}`, this.props.className)} style={this.props.style} aria-hidden="true"/>
-    )
+function StatusIcon({status, className, style}) {
+  const statusToIcon = {
+    toDo: 'list-alt',
+    inProgress: 'star text-warning',
+    waiting: 'clock-o',
+    done: 'check'
   }
+  const icon = statusToIcon[status || 'toDo']
+  return (
+    <i className={classNames('fa', 'fa-lg', `fa-${icon}`, className)} style={style} aria-hidden="true"/>
+  )
 }
+
+StatusIcon.propTypes = {
+  status: PropTypes.string,
+  className: PropTypes.string,
+  style: PropTypes.object,
+};
+
+export default StatusIcon
