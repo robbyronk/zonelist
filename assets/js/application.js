a19d6f6d36feaa84f83dcf28c4b157c56d4724c7
Robby Ronk <robby.ronk@gmail.com>
2017-09-23 14:58:01 +1200

    Catch exceptions with Sentry

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,6 +5,10 @@ import configureStore, {sagaMiddleware} from "./store";
 import Root from "./root";
 import rootSaga from "./sagas";
 
+if (process.env.NODE_ENV === 'production') {
+  Raven.config('https://4b8dc207376b44d3a8dd047e56cf03e3@sentry.io/221104').install()
+}
+
 const store = configureStore();
 sagaMiddleware.run(rootSaga)
 
