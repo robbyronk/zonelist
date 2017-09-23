d94882b52f61462fe89f7461a263e36538eda66f
Robby Ronk <robby.ronk@gmail.com>
2017-09-24 11:10:33 +1300

    cleanup

--- assets/js/components/footer.js
+++ assets/js/components/footer.js
@@ -2,16 +2,18 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {showFocus, showOutline} from '../actions'
 
-function Footer({view, showFocus, showOutline}) {
-  const outlineMode = ( <span onClick={() => showFocus()}> Outline Mode <i className="fa fa-toggle-on fa-rotate-180 fa-lg text-primary"/> Focus Mode </span> )
-  const focusMode = ( <span onClick={() => showOutline()}> Outline Mode <i className="fa fa-toggle-on fa-lg text-success"/> Focus Mode </span> )
+const Footer = ({view, showFocus, showOutline}) => {
+  const outlineMode = (
+    <span onClick={showFocus}> Outline Mode <i className="fa fa-toggle-on fa-lg fa-rotate-180 text-primary"/> Focus Mode </span> )
+  const focusMode = (
+    <span onClick={showOutline}> Outline Mode <i className="fa fa-toggle-on fa-lg text-success"/> Focus Mode </span> )
   return (
     <footer className="footer mt-4 bg-faded fixed-bottom">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-auto">
-            { view === 'outline' ? outlineMode : null }
-            { view === 'focus' ? focusMode : null }
+            {view === 'outline' ? outlineMode : null}
+            {view === 'focus' ? focusMode : null}
           </div>
         </div>
       </div>
@@ -19,4 +21,4 @@ function Footer({view, showFocus, showOutline}) {
   )
 }
 
-export default connect((state) => ({view: state.view}), {showOutline, showFocus})(Footer)
+export default connect(state => ({view: state.view}), {showOutline, showFocus})(Footer)
