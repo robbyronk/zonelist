761f6bb830d79aa7ad6a5481e7cfe04d5bcfcece
Robby Ronk <robby.ronk@gmail.com>
2017-09-23 18:48:25 +1200

    Big cleanup

--- assets/js/sagas/socket.js
+++ assets/js/sagas/socket.js
@@ -6,74 +6,56 @@ import {call, fork, put, take} from "redux-saga/effects";
 import ActionTypes, {removeItem, updateTask} from '../actions'
 import {fetchTasks} from "./items";
 
-function* connectToSocket(url) {
-  const socket = new Socket(url, {
+function createEventChannel(phoenixChannel, phoenixEventName) {
+  return eventChannel(emit => {
+    const doEmit = payload => emit({payload})
+
+    phoenixChannel.on(phoenixEventName, doEmit);
+
+    return () => phoenixChannel.off(phoenixEventName, doEmit);
+  });
+}
+
+function *watchTaskCreate(phoenixChannel){
+  const channel = createEventChannel(phoenixChannel, 'task_create')
+  while (true) {
+    yield take(channel)
+    yield call(fetchTasks)
+  }
+}
+
+function *watchTaskUpdate(phoenixChannel){
+  const channel = createEventChannel(phoenixChannel, 'task_update')
+  while (true) {
+    const event = yield take(channel)
+    yield put(updateTask(event.payload));
+  }
+}
+
+function *watchTaskDelete(phoenixChannel){
+  const channel = createEventChannel(phoenixChannel, 'task_delete')
+  while (true) {
+    const event = yield take(channel)
+    yield put(removeItem(event.payload.id, true));
+  }
+}
+
+function* startSocket(action) {
+  const {session} = action;
+
+  const socket = new Socket('/socket', {
     params: {token: localStorage.getItem('id_token')},
     logger: (kind, msg, data) => {
       console.log(`${kind}: ${msg}`, data)
     },
   });
   socket.connect();
-  return socket;
-}
+  const phoenixChannel = socket.channel(`users:${session.user}`, {})
+  phoenixChannel.join();
 
-// TODO channel.join is async, this is probably an error
-function* joinPhoenixChannel(socket, channel_name) {
-  const channel = socket.channel(channel_name, {});
-  channel.join()
-    .receive("ok", ({messages}) => console.log("catching up", messages))
-    .receive("error", ({reason}) => console.log("failed join", reason))
-    .receive("timeout", () => console.log("Networking issue. Still waiting..."))
-  return channel;
-}
-
-function createEventChannel(phoenixChannel) {
-  return eventChannel(emit => {
-    const emitTaskCreate = () => {
-      emit({type: 'task_create'})
-    }
-    const emitTaskUpdate = (task) => {
-      emit({type: 'task_update', task})
-    }
-    const emitTaskDelete = (task) => {
-      emit({type: 'task_delete', task})
-    }
-
-    phoenixChannel.on('task_create', emitTaskCreate);
-    phoenixChannel.on('task_update', emitTaskUpdate);
-    phoenixChannel.on('task_delete', emitTaskDelete);
-
-    return () => {
-      phoenixChannel.off('task_create', emitTaskCreate)
-      phoenixChannel.off('task_update', emitTaskUpdate)
-      phoenixChannel.off('task_delete', emitTaskDelete)
-    };
-  });
-}
-
-function* startSocket(action) {
-  const {session} = action;
-
-  const socket = yield call(connectToSocket, '/socket');
-  const channelName = `users:${session.user}`;
-  const phoenixChannel = yield call(joinPhoenixChannel, socket, channelName);
-
-  const sagaChannel = yield call(createEventChannel, phoenixChannel);
-
-  while (true) {
-    const event = yield take(sagaChannel)
-    switch (event.type) {
-      case 'task_create':
-        yield call(fetchTasks)
-        break
-      case 'task_update':
-        yield put(updateTask(event.task));
-        break
-      case 'task_delete':
-        yield put(removeItem(event.task.id))
-        break
-    }
-  }
+  yield fork(watchTaskCreate, phoenixChannel)
+  yield fork(watchTaskUpdate, phoenixChannel)
+  yield fork(watchTaskDelete, phoenixChannel)
 }
 
 export default function* getWatcher() {
