9174d7f8ee1fa899e1336de535f9f8df143a9905
Robby Ronk <robby.ronk@gmail.com>
2017-09-25 21:09:08 +1300

    Find context with lodash works again

--- assets/js/selectors/findContext.js
+++ assets/js/selectors/findContext.js
@@ -1,29 +1,10 @@
-// todo try to use lodash again, webpack or babel couldn't compile this correctly
-// const findContext = (items, toFind, acc = []) => {
-//   acc.unshift(toFind)
-//   if (toFind === 'root') {
-//     return acc
-//   }
-//   return findContext(items, find(items, i => includes(i.children, toFind)).id, acc)
-// }
+import {find, includes} from 'lodash'
 
-// todo make this with lodash
-function findContext(items, toFind) {
-  // console.log(arguments)
-  let target = toFind
-  let acc = [target]
-  let limit = 100
-  while (!items[target].root && limit > 0) { // loop until we are trying to find the parent of root
-    for(let item of Object.values(items)) { // loop through all tasks
-      let indexOf = item.children.indexOf(target)
-      if(indexOf !== -1) { // found a task that has the target in its children
-        target = item.id // new target is the parent
-        acc.unshift(target)
-        break // break for loop
-      }
-    }
-    limit--;
+const findContext = (items, toFind, acc = []) => {
+  acc.unshift(toFind)
+  if (items[toFind].root) {
+    return acc
   }
-  return acc
+  return findContext(items, find(items, i => includes(i.children, toFind)).id, acc)
 }
 export default findContext
