81e270c8a887304cd25caf3b8d7195948e3a2c59
Robby Ronk <robby.ronk@gmail.com>
2017-09-28 22:28:49 +1300

    Delete row class to remove margins which removes the scroll bar for the page, making it actually 100vh also delete weird code

--- assets/js/components/footer.js
+++ assets/js/components/footer.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import classnames from 'classnames'
 import {showFocus, showOutline} from '../actions'
 
 const Footer = ({view, showFocus, showOutline}) => {
-  const outlineMode = (
-    <span onClick={showFocus}> Outline Mode <i className="fa fa-toggle-on fa-lg fa-rotate-180 text-primary"/> Focus Mode </span> )
-  const focusMode = (
-    <span onClick={showOutline}> Outline Mode <i className="fa fa-toggle-on fa-lg text-success"/> Focus Mode </span> )
+  const iconClassName = classnames(
+    'fa', 'fa-toggle-on', 'fa-lg',
+    {
+      'fa-rotate-180': view === 'outline',
+      'text-primary': view === 'outline',
+      'text-success': view === 'focus',
+    }
+  );
   return (
-    <footer className="footer bg-faded">
-      <div className="row justify-content-center">
-        <div className="col-md-auto">
-          {view === 'outline' ? outlineMode : null}
-          {view === 'focus' ? focusMode : null}
-        </div>
+    <footer className="footer bg-faded d-flex flex-row justify-content-center">
+      <div onClick={view === 'outline' ? showFocus : showOutline}>
+        Outline Mode <i className={iconClassName}/> Focus Mode
       </div>
     </footer>
   )
