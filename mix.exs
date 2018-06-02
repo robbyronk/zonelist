84b935fb8c7957edc210146f2dd40eab3cb1a32f
Robby Ronk <robby.ronk@gmail.com>
2018-06-03 10:38:21 +1200

    Run ecto migrations and seeds during zone.setup

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Zone.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["zone.setup": ["deps.get", "zone.frontend", "ecto.create", "run priv/repo/seeds.exs"],
+    ["zone.setup": ["deps.get", "zone.frontend", "ecto.setup", "run priv/repo/seeds.exs"],
      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
      "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
