7b65bda9f8c502c03cb97a0967d3c813f3d8dd9d
robbyronk <robby.ronk@gmail.com>
2018-06-03 18:05:20 +1200

    Don't try to compile the phoenix deps on MacOS MacOS does something with symlinks and it's weird

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /(node_modules|phoenix)/,
         query: {
           cacheDirectory: true,
           plugins: ['transform-decorators-legacy', "react-hot-loader/babel"],
